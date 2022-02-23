4c72f0bba63d448c449904f31b77bf5659421890
VENKATA NAGA FANENDRA YADLAPALLI <fanendra.yadlapalli@gmail.com>
2022-02-23 22:13:22 +0530

    Time: 8 ms (37.25%), Space: 42.8 MB (16.89%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.java
+++ 97-interleaving-string/97-interleaving-string.java
@@ -6,24 +6,20 @@ class Solution {
         if(res!=m+n)
             return false;
         boolean dp[][] = new boolean[m+1][n+1];
-        for(int i=0;i<=m;i++){
-            for(int j=0;j<=n;j++){
-                if(i==0 && j==0)
-                    dp[i][j]=true;
-                else if(i==0){
-                    if(s3.charAt(i+j-1) == s2.charAt(j-1))
-                        dp[i][j]=dp[i][j-1];
-                } 
-                else if(j==0){
-                     if(s3.charAt(i+j-1) == s1.charAt(i-1))
-                         dp[i][j]=dp[i-1][j];
-                }   
-                else {
+        dp[0][0]=true;
+        for(int i=1;i<=m;i++)
+            if(s3.charAt(i-1) == s1.charAt(i-1))
+                        dp[i][0]=dp[i-1][0];
+        for(int j=1;j<=n;j++)
+           if(s3.charAt(j-1) == s2.charAt(j-1))
+                         dp[0][j]=dp[0][j-1]; 
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
                     if(s3.charAt(i+j-1)==s1.charAt(i-1))
                     dp[i][j]=dp[i-1][j];
                     if(s3.charAt(i+j-1)==s2.charAt(j-1))
                     dp[i][j]= dp[i][j] || dp[i][j-1];
-                }
+
             }
         }
         return dp[m][n];
