e1e003218a71a6caf473e5c4fe9427c0745f5f90
VENKATA NAGA FANENDRA YADLAPALLI <fanendra.yadlapalli@gmail.com>
2022-02-23 22:06:46 +0530

    Time: 7 ms (44.06%), Space: 42.2 MB (32.63%) - LeetHub

---
+++ 97-interleaving-string/97-interleaving-string.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int m = s1.length();
+        int n = s2.length();
+        int res = s3.length();
+        if(res!=m+n)
+            return false;
+        boolean dp[][] = new boolean[m+1][n+1];
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
+                if(i==0 && j==0)
+                    dp[i][j]=true;
+                else if(i==0){
+                    if(s3.charAt(i+j-1) == s2.charAt(j-1))
+                        dp[i][j]=dp[i][j-1];
+                } 
+                else if(j==0){
+                     if(s3.charAt(i+j-1) == s1.charAt(i-1))
+                         dp[i][j]=dp[i-1][j];
+                }   
+                else {
+                    if(s3.charAt(i+j-1)==s1.charAt(i-1))
+                    dp[i][j]=dp[i-1][j];
+                    if(s3.charAt(i+j-1)==s2.charAt(j-1))
+                    dp[i][j]= dp[i][j] || dp[i][j-1];
+                }
+            }
+        }
+        return dp[m][n];
+        
+    }
+}
