3791d4d5a5aecdf81fad20ef95226e1bf5e6ec96
VENKATA NAGA FANENDRA YADLAPALLI <fanendra.yadlapalli@gmail.com>
2022-03-24 10:35:42 +0530

    Time: 59 ms (91.54%), Space: 77 MB (56.19%) - LeetHub

---
+++ 1818-minimum-absolute-sum-difference/1818-minimum-absolute-sum-difference.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minAbsoluteSumDiff(int[] nums1, int[] nums2) {
+        int sorted[] = nums1.clone();
+        Arrays.sort(sorted);
+        long sumDiff = 0;
+        int mx=Integer.MIN_VALUE;
+        for(int i=0;i<nums2.length;i++){
+            int diff = Math.abs(nums1[i]-nums2[i]);
+            sumDiff+=diff;
+            int index = Arrays.binarySearch(sorted,nums2[i]);
+            if(index<0)
+                index=-1*(index+1);
+            if(index<sorted.length)
+                mx=Math.max(mx,diff-Math.abs(sorted[index]-nums2[i]));
+            if(index>0)
+                mx=Math.max(mx,diff-Math.abs(sorted[index-1]-nums2[i]));
+        }
+        return (int)((sumDiff-mx)%1000000007);
+    }
+}
